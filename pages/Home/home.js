d06eeb588c7dc893b2cd6f2345703cae49a14d0f
zhanghao03061 <zhanghao@tcl.com>
2018-04-09 11:52:55 +0800

    Signed-off-by: zhanghao03061 <zhanghao@tcl.com>

--- pages/Home/home.js
+++ pages/Home/home.js
@@ -2,10 +2,10 @@ var app = getApp()
 Page({
   data: {
     movies: [
-      { id: 1,url: '../../Picture/1.jpg'  },
-      { id: 2,url: '../../Picture/2.jpg' },
-      { id: 3,url: '../../Picture/7.jpg' },
-      { id: 4,url: '../../Picture/8.jpg' }
+      { id: 1, url: 'http://btdxcx.com:3012/image/5607929E108EA5EA'  },
+      // { id: 2,url: '../../Picture/2.jpg' },
+      // { id: 3,url: '../../Picture/7.jpg' },
+      // { id: 4,url: '../../Picture/8.jpg' }
     ]
   },
 
